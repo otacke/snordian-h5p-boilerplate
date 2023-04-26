1a33c783b34b567dc5a95c897776668e307f34c3
Oliver Tacke <o.tacke@posteo.de>
2023-04-26 14:06:45 +0200

    Simplify build chain

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-const nodeEnv = process.env.NODE_ENV || 'development';
+const mode = process.argv.includes('--mode=production') ?
+  'production' : 'development';
 const libraryName = process.env.npm_package_name;
 
 module.exports = {
-  mode: nodeEnv,
+  mode: mode,
   resolve: {
     alias: {
       '@scripts': path.resolve(__dirname, 'src/scripts'),
@@ -15,7 +16,7 @@ module.exports = {
     }
   },
   optimization: {
-    minimize: nodeEnv === 'production',
+    minimize: mode === 'production',
     minimizer: [
       new TerserPlugin({
         terserOptions: {
@@ -78,5 +79,5 @@ module.exports = {
   stats: {
     colors: true
   },
-  ...(nodeEnv !== 'production' && { devtool: 'eval-cheap-module-source-map' })
+  ...(mode !== 'production' && { devtool: 'eval-cheap-module-source-map' })
 };
